fc08d0d6b74a18264ad04315b990a3e23232ac15
gill-sumit <sumit.gupta@gillbroking.com>
2023-06-23 17:38:52 +0530

    change method

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const redirect_url = window.location.href;
-  const [data, setData] = useState(null);
+  const redirect_url = window.location.pathname;
+  const [data, setData] = useState(window.location.pathname);
 
   const fetchData = async () => {
     try {
@@ -18,33 +18,39 @@ function App() {
       console.error(error);
     }
   };
+useEffect(() => {
+  console.log("===", data)
+}, [data])
+  const handleUrlChange = () => {
+    // fetchData();
+    if (window.location.pathname === `${redirect_url}call`) {
+      alert("callback")
+    }
+    else alert(window.location.pathname)
+    // Your code logic here
+    console.log('URL has changed:', window.location.pathname);
+  };
 
   useEffect(() => {
-    const handleUrlChange = () => {
-      fetchData();
-      if (window.location.pathname === `${redirect_url}call`)
-        alert("callback")
-      else alert(window.location.pathname)
-      // Your code logic here
-      console.log('URL has changed:', window.location.pathname);
-    };
 
-    window.addEventListener('popstate', handleUrlChange);
+
+    window.addEventListener('locationchange', handleUrlChange);
 
     // Clean up the event listener
     return () => {
-      window.removeEventListener('popstate', handleUrlChange);
+      window.removeEventListener('locationchange', handleUrlChange);
     };
   }, []);
   const handleRedirect = () => {
-    var stateid = 'signup' + Math.random().toString(36).substring(7);
+    var stateid = 'rmsign' + Math.random().toString(36).substring(7);
     sessionStorage.setItem('state', stateid);
     window.location.href =
-        'https://api.digitallocker.gov.in/public/oauth2/1/authorize?response_type=code&client_id=IAE3E4C164&state=' +
-        stateid +
-        '';
+      'https://api.digitallocker.gov.in/public/oauth2/1/authorize?response_type=code&client_id=IAE3E4C164&state=' +
+      stateid +'&redirect_uri=https://first.d1ds8gytdtrzs9.amplifyapp.com/call';
+
+    // }
   }
-  
+
   return (
     <div className="App">
       <header className="App-header">
